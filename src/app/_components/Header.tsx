a46891acc6968033e57cc90d157dae379d6121eb
M. Aji Perdana <ajicooljazz38@gmail.com>
2024-07-20 16:19:26 +0700

    feat: update animation ✅🚀

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,24 +1,57 @@
-import { CardBody, CardItem } from "@/components/3d-card";
+import React from "react";
 import { BackgroundBeams } from "@/components/background-beams";
 import { FlipWords } from "@/components/flip-words";
 import { HoverBorderGradient } from "@/components/hover-border-gradient";
-import Image from "next/image";
-import React from "react";
+import { motion } from "framer-motion";
 
 const Header: React.FC = (): JSX.Element => {
-  const words: string[] = ["Web", "Backend", "Photographer"];
+  const words: string[] = [
+    "Web Developer",
+    "Backend Developer",
+    "Photographer",
+  ];
 
   return (
     <header className="container p-4 lg:p-0 antialiased w-full h-screen flex justify-center items-center">
-      <div className="w-full z-10 text-center flex flex-col gap-y-3 font-Playfair_Display">
-        <p className="text-lg lg:text-5xl">✨ Hello i{"'"}m</p>
-        <h1 className="text-4xl lg:text-9xl font-bold">
-          M. <span className="text-yellow-400">AJI</span> PERDANA
-        </h1>
-        <div className="text-xl lg:text-5xl font-bold my-4">
+      <div className="w-full z-10 text-center flex flex-col justify-center items-center gap-y-3 font-Playfair_Display">
+        <motion.p
+          initial={{ y: -100, opacity: 0, scale: 0.3 }}
+          animate={{ y: 0, opacity: 1, scale: [0.1, 1] }}
+          transition={{
+            delay: 0.3,
+            duration: 1,
+            ease: "backOut",
+          }}
+          className="text-lg lg:text-5xl"
+        >
+          ✨ Hello i{"'"}m
+        </motion.p>
+        <motion.h1
+          initial={{ y: -30, scale: 0.9, opacity: 0 }}
+          animate={{ y: 0, scale: 1, opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.8, ease: "circOut" }}
+          className="text-4xl lg:text-9xl font-bold"
+        >
+          M. <span className="text-amber-400">AJI</span> PERDANA
+        </motion.h1>
+        <motion.div
+          initial={{ y: -20, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ delay: 0.2, duration: 0.8, ease: "backOut" }}
+          className="text-xl lg:text-5xl font-bold my-4"
+        >
           I{"'"}m a <FlipWords {...{ words }} />
-        </div>
-        <div className="flex justify-center mb-10 mt-4">
+        </motion.div>
+        <motion.div
+          initial={{ y: 100, opacity: 0, scale: 0 }}
+          animate={{ y: 0, opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.8,
+            ease: "backOut",
+          }}
+          whileHover={{ scale: 1.2 }}
+          className="flex justify-center mb-10 mt-4 w-fit"
+        >
           <HoverBorderGradient
             containerClassName="rounded-full"
             as="button"
@@ -26,7 +59,7 @@ const Header: React.FC = (): JSX.Element => {
           >
             <span>Contact Me</span>
           </HoverBorderGradient>
-        </div>
+        </motion.div>
       </div>
       <BackgroundBeams />
     </header>
